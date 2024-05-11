84f09f8e6581950f710e4843be6b3ac5b7016dba
EsauVargasGit <124740759+EsauVargasGit@users.noreply.github.com>
2024-05-11 14:13:28 -0600

    Adding comments

--- IaC-ec2/ec2_keypair.tf
+++ IaC-ec2/ec2_keypair.tf
@@ -1,15 +1,18 @@
+# Configuración para la creación de una key_pair de la EC2
+
+# RSA key para 4096 bits 
 resource "tls_private_key" "key_pair" {
   algorithm = "RSA"
   rsa_bits  = 4096
 }
 
-
+# Configuración de la key_pair
 resource "aws_key_pair" "key_pair" {
   key_name   = "sonar-key-pair"
   public_key = tls_private_key.key_pair.public_key_openssh
 }
 
-
+# Creación de .pem que contiene los valores de la key_pair
 resource "local_file" "ssh_key" {
   filename = "${aws_key_pair.key_pair.key_name}.pem"
   content  = tls_private_key.key_pair.private_key_pem
