43f2c879e917c4ccf64eca62c7e96c89fee630c8
EsauVargasGit <124740759+EsauVargasGit@users.noreply.github.com>
2024-05-11 14:31:49 -0600

    Update vars.tf

--- IaC-ec2/vars.tf
+++ IaC-ec2/vars.tf
@@ -1,7 +1,11 @@
+# Variables
+
+# Variable de la región a utilizar
 variable "AWS_REGION" {
   default = "us-east-1"
 }
 
+# ID de la AMI a utilizar para la creación de la EC2 con SonarQube
 variable "ami" {
   type = map(any)
   default = {
